a59678db3d988bb9574d9e18a7f418102208002f
Sameer Giri <sameergiri1997@gmail.com>
2018-08-13 01:24:47 +0530

    Added autoscrolling

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,16 @@ let submit = document.querySelector("#go");
 let messageField = document.querySelector("#message-field");
 let text = document.querySelector("#text-area");
 let sendLocationBtn = document.querySelector("#send-location");
+
+function scrollToBottom() {
+    let messageFieldChildLen = messageField.children.length;
+    let newMessage = messageField.children[messageFieldChildLen-1];
+    let newMessageInnerHt = newMessage.clientHeight;
+    let clienHt = messageField.clientHeight
+    let scrollTopHt = messageField.scrollTop
+    let scrollHt = messageField.scrollHeight
+    messageField.scrollTop = scrollHt + newMessageInnerHt
+}
 messageForm.addEventListener('submit', function (e) {
     e.preventDefault();
     socket.emit('createMessage', {
@@ -15,19 +25,21 @@ messageForm.addEventListener('submit', function (e) {
 
 sendLocationBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    let ua = navigator.userAgent.toLowerCase(),
-    isAndroid = ua.indexOf("android") > -1,
-    geoTimeout = isAndroid ? '15000' : '1000';
     if('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(position.coords.latitude, position.coords.longitude);
             let locationLink = `https://www.google.co.in/maps/place/ ${position.coords.latitude},${position.coords.longitude}`;
             socket.emit('createLocation', {
                 from: "Admin",
                 locationURL: `${locationLink}`
             });
-        }, function () {alert('Unable to access location')},
-         {enableHighAccuracy: true, maximumAge: 3000, timeout:geoTimeout});
+        }, 
+        function () {alert('Unable to access location')});
     }
+    else {
+        alert(`Your browser doesn't have location support!`);
+    }
+    scrollToBottom()
 });
 
 socket.on('connect', function() {
@@ -36,10 +48,12 @@ socket.on('connect', function() {
 
 socket.on('newMessage', function (message) {
     messageField.insertAdjacentHTML('beforeend', `<div><p>${message.from}: ${message.text}</p></div>`);
+    scrollToBottom();
 });
 
 socket.on('newLocation', function (locationInfo) {
     messageField.insertAdjacentHTML('beforeend', `<div><p>${locationInfo.from}: <a href='${locationInfo.locationURL}' target='_blank'>User has shared his location</a></div>`);
+    scrollToBottom();
 });
 
 socket.on('disconnect', function() {
